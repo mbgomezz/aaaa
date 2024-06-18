93e02804a732ea8a99efeadcb619fbee9d5f616b
mbgomezz <mb.gomez@duocuc.cl>
2024-06-17 21:51:15 -0400

    act 2

---
+++ 3.1.2 ActividadListas(2).py
@@ -0,0 +1,4 @@
+#2) Cree 2 listas, en las cuales se guardará 3 nombres y 3 apellidos (1 lista para nombres y una 1 lista para apellidos), 
+#el sistema deberá mostrar de forma ordenada los nombres y apellidos.
+lista_nombres = []
+lista_apellidos = []
